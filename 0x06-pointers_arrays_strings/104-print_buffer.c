892a741803957c4bc6c53c39f3d38233c336052d
Abdourahamane <190601016@rdu.edu.tr>
2023-03-31 14:27:42 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -19,7 +19,7 @@ void print_buffer(char *b, int size)
 	}
 	while (o < size)
 	{
-		j = size - o < 10? size - o: 10;
+		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
 		for (i = 0; i < 10; i++)
 		{
@@ -34,8 +34,8 @@ void print_buffer(char *b, int size)
 		}
 		for (i = 0; i < j; i++)
 		{
-			int c = *(b+ o + i);
-			
+			int c = *(b + o + i);
+
 			if (c < 32 || c > 132)
 			{
 				c = '.';
