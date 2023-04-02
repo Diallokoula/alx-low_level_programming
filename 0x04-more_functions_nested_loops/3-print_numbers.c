8f2ce31f9286b9a86720d043710e8f6db287a5cf
Abdourahamane <190601016@rdu.edu.tr>
2023-04-02 06:56:18 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
- * return: void
+ * print_numbers - prints numbers between 0-9.
+ * Return: no return.
  */
 
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c >= '9'; c++)
+	for (c = 48; c >= 58; c++)
 	{
 		_putchar(c);
 	}
